36212f10d2d43204d93f4c30b1706f8164ffa299
premalathas1 <premalathas@rvsgroup.com>
2026-08-21 14:58:53 +0530

    Create student_solution.sql

---
+++ student_solution.sql
@@ -0,0 +1,17 @@
+-- =========================================
+-- SQL Assignment: ALTER TABLE
+-- Name:
+-- Register Number:
+-- =========================================
+
+-- The Student table already exists.
+
+-- Add the following columns:
+-- Email       VARCHAR(30)
+-- PhoneNumber INT(10)
+
+-- Write your ALTER TABLE statement below:
+
+
+-- Display the modified table structure:
+
